765d353bcd6146e67270e49daee3573a0b18451e
Brendon Page <brendon.page@chillisoft.co.za>
2021-04-23 15:52:06 +0200

    Added itemTestDataBuilder.

---
+++ src/testUtils/itemTestDataBuilder.ts
@@ -0,0 +1,24 @@
+import { Item } from "../dependencies/Item"
+
+export class ItemTestDataBuilder {
+  private price: number | null = null;
+
+  static create() {
+    return new ItemTestDataBuilder()
+  }
+
+  withPrice(value: number) {
+    this.price = value
+    return this;
+  }
+
+  build(): Item {
+    return {
+      id: fakerStatic.random.alphaNumeric(10),
+      name: fakerStatic.random.words(),
+      price: this.price ?? fakerStatic.finance.amount(),
+      description: fakerStatic.random.words(),
+      created: fakerStatic.date.future()
+    } as unknown as Item
+  }
+}
